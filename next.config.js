44755c937271cc1e711f194962570222d40002a6
G. Cappai <cappaigianluca@gmail.com>
2021-12-08 01:44:02 +0100

    Setup for mdx based pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const withMDX = require('@next/mdx')()
+
+const nextConfig = {
     reactStrictMode: true,
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
+
+module.exports = withMDX(nextConfig)
