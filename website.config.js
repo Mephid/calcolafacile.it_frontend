b3dcefed82a9881ded89d11d90600b59475084ee
G. Cappai <cappaigianluca@gmail.com>
2021-12-18 20:25:28 +0100

    Update API_URL

--- website.config.js
+++ website.config.js
@@ -1,13 +1,18 @@
 const BASE_URL =
     process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000'
+        : 'https://calcolafacile.com'
+
+const API_BASE_URL =
+    process.env.NODE_ENV === 'development'
         ? 'http://localhost:5000'
         : 'https://calcolafacile-backend.herokuapp.com'
 
 const config = {
     SITE_NAME: 'Calcolafacile.it',
     BASE_URL: BASE_URL,
-    CALCULATION_SERVER_API_URL: BASE_URL + '/api/calculators',
-    CONTACTS_SERVER_API_URL: BASE_URL + '/api/contact',
+    CALCULATION_SERVER_API_URL: API_BASE_URL + '/api/calculators',
+    CONTACTS_SERVER_API_URL: API_BASE_URL + '/api/contact',
 }
 
 export default config
