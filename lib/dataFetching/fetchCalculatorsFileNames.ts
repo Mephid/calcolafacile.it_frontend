e924106b61f39cfa01c00f77fef83110884dbbbc
G. Cappai <cappaigianluca@gmail.com>
2021-12-19 12:13:30 +0100

    Fix folder name

--- lib/dataFetching/fetchCalculatorsFileNames.ts
+++ lib/dataFetching/fetchCalculatorsFileNames.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 
 const fetchCalculatorsFileNames = async () => {
-    const calculatorPath = path.join(process.cwd(), 'pages', 'calcolatori')
+    const calculatorPath = path.join(process.cwd(), 'pages', 'calcoli')
 
     const calculatorsFileNames = await fs.promises.readdir(calculatorPath)
 
