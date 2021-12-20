3500d50ff6d265bc56d3f3e7e2dbca16592ef8f9
G. Cappai <cappaigianluca@gmail.com>
2021-12-20 13:34:59 +0100

    Fix memory leak on fetching server results

--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState, useReducer } from 'react'
+import { useReducer, useEffect, useMemo } from 'react'
 
 const errorMessage = {
     FETCH: 'Si è verificato un errore.',
@@ -12,61 +12,74 @@ type useFetchOutput = [
     boolean
 ]
 
-const useFetch = (url: string, method?: string): useFetchOutput => {
-    const reducer = (
-        state: {
-            error: string
-            result: Record<any, any>
-            isLoading: boolean
-        },
-        action: Record<any, any>
-    ) => {
-        switch (action.type) {
-            case 'SUCCESS':
-                return {
-                    error: '',
-                    result: action.payload.result,
-                    isLoading: false,
-                }
-            case 'FETCH_ERROR':
-                return {
-                    error: errorMessage.FETCH,
-                    result: {},
-                    isLoading: false,
-                }
-            case 'SERVER_ERROR':
-                return {
-                    error: errorMessage.SERVER,
-                    result: {},
-                    isLoading: false,
-                }
-            case 'LOADING':
-                return {
-                    error: '',
-                    result: {},
-                    isLoading: true,
-                }
-            default:
-                return state
-        }
+const reducer = (
+    state: {
+        error: string
+        result: Record<any, any>
+        isLoading: boolean
+    },
+    action: Record<any, any>
+) => {
+    switch (action.type) {
+        case 'SUCCESS':
+            return {
+                error: '',
+                result: action.payload.result,
+                isLoading: false,
+            }
+        case 'FETCH_ERROR':
+            return {
+                error: errorMessage.FETCH,
+                result: {},
+                isLoading: false,
+            }
+        case 'SERVER_ERROR':
+            return {
+                error: errorMessage.SERVER,
+                result: {},
+                isLoading: false,
+            }
+        case 'LOADING':
+            return {
+                error: '',
+                result: {},
+                isLoading: true,
+            }
+        default:
+            return state
     }
+}
 
+const useFetch = (url: string, method?: string): useFetchOutput => {
     const [state, dispatch] = useReducer(reducer, {
         error: '',
         result: {},
         isLoading: false,
     })
 
-    const reqOptions: {
-        method: string
-        headers: Record<string, string>
-        body?: string
-    } = {
-        method: method || 'GET',
-        headers: { 'Content-Type': 'application/json' },
-    }
+    const controller = useMemo(() => new AbortController(), [])
+    const { signal } = controller
+
+    useEffect(() => {
+        return () => {
+            if (controller) {
+                controller.abort()
+            }
+        }
+    }, [controller])
 
     const doFetch = async (body?: Record<any, any>) => {
+        console.log('recreating function')
+
+        const reqOptions: {
+            method: string
+            headers: Record<string, string>
+            body?: string
+        } = {
+            method: method || 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        }
+
         if (body) {
             reqOptions.body = JSON.stringify(body)
         }
@@ -74,19 +87,23 @@ const useFetch = (url: string, method?: string): useFetchOutput => {
         dispatch({ type: 'LOADING' })
 
         try {
-            const response = await fetch(url, reqOptions)
+            const response = await fetch(url, { signal, ...reqOptions })
 
-            if (response.ok) {
-                const responseObject = await response.json()
-                dispatch({
-                    type: 'SUCCESS',
-                    payload: { result: responseObject },
-                })
-            } else {
-                dispatch({ type: 'SERVER_ERROR' })
+            if (response) {
+                if (response.ok) {
+                    const responseObject = await response.json()
+                    dispatch({
+                        type: 'SUCCESS',
+                        payload: { result: responseObject },
+                    })
+                } else {
+                    dispatch({ type: 'SERVER_ERROR' })
+                }
             }
         } catch (err) {
-            dispatch({ type: 'FETCH_ERROR' })
+            if (!(err instanceof DOMException)) {
+                dispatch({ type: 'FETCH_ERROR' })
+            }
         }
     }
 
